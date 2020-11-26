c849256dc10fb0e089e8d8bf9bf2bab9363cf0fb
Luiz Felipe <72635266+LuizNascimento442@users.noreply.github.com>
2020-11-26 14:28:10 -0300

    Add files via upload

---
+++ CadastroMvc/CadastroMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CadastroMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
